71c7581e43972c0f1ab2d9e2129022c115411f94
Jon <jon.hysenaj@leerling.kov.be>
2025-01-23 10:04:50 +0100

    buttons en textbox aangemaakt (basis) moet nog layout hebben

--- Frontend/RegistratieProgram/Form1.Designer.cs
+++ Frontend/RegistratieProgram/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RegistratieProgram
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Titel = new System.Windows.Forms.Label();
+            this.voornaam_textbox = new System.Windows.Forms.TextBox();
+            this.leeftijd_textbox = new System.Windows.Forms.TextBox();
+            this.achternaam_textbox = new System.Windows.Forms.TextBox();
+            this.email_textbox = new System.Windows.Forms.TextBox();
+            this.gsm_textbox = new System.Windows.Forms.TextBox();
+            this.uitleg_game = new System.Windows.Forms.Label();
+            this.Registratie_text = new System.Windows.Forms.Label();
+            this.voornaam_label = new System.Windows.Forms.Label();
+            this.achternaam_label = new System.Windows.Forms.Label();
+            this.email_label = new System.Windows.Forms.Label();
+            this.leeftijd_label = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.gsm_label = new System.Windows.Forms.Label();
+            this.registratie_btn = new System.Windows.Forms.Button();
+            this.gsm_pakker_als_er_mooie_mamas_zijn = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Titel
+            // 
+            this.Titel.AutoSize = true;
+            this.Titel.BackColor = System.Drawing.Color.Transparent;
+            this.Titel.Location = new System.Drawing.Point(12, 9);
+            this.Titel.Name = "Titel";
+            this.Titel.Size = new System.Drawing.Size(190, 16);
+            this.Titel.TabIndex = 0;
+            this.Titel.Text = "Welkom bij onze TechoV-Quiz!";
+            // 
+            // voornaam_textbox
+            // 
+            this.voornaam_textbox.Location = new System.Drawing.Point(320, 103);
+            this.voornaam_textbox.Name = "voornaam_textbox";
+            this.voornaam_textbox.Size = new System.Drawing.Size(100, 22);
+            this.voornaam_textbox.TabIndex = 1;
+            // 
+            // leeftijd_textbox
+            // 
+            this.leeftijd_textbox.Location = new System.Drawing.Point(320, 191);
+            this.leeftijd_textbox.Name = "leeftijd_textbox";
+            this.leeftijd_textbox.Size = new System.Drawing.Size(100, 22);
+            this.leeftijd_textbox.TabIndex = 2;
+            // 
+            // achternaam_textbox
+            // 
+            this.achternaam_textbox.Location = new System.Drawing.Point(320, 147);
+            this.achternaam_textbox.Name = "achternaam_textbox";
+            this.achternaam_textbox.Size = new System.Drawing.Size(100, 22);
+            this.achternaam_textbox.TabIndex = 3;
+            // 
+            // email_textbox
+            // 
+            this.email_textbox.Location = new System.Drawing.Point(320, 235);
+            this.email_textbox.Name = "email_textbox";
+            this.email_textbox.Size = new System.Drawing.Size(100, 22);
+            this.email_textbox.TabIndex = 4;
+            // 
+            // gsm_textbox
+            // 
+            this.gsm_textbox.Location = new System.Drawing.Point(320, 279);
+            this.gsm_textbox.Name = "gsm_textbox";
+            this.gsm_textbox.Size = new System.Drawing.Size(100, 22);
+            this.gsm_textbox.TabIndex = 5;
+            // 
+            // uitleg_game
+            // 
+            this.uitleg_game.AutoSize = true;
+            this.uitleg_game.Location = new System.Drawing.Point(30, 81);
+            this.uitleg_game.Name = "uitleg_game";
+            this.uitleg_game.Size = new System.Drawing.Size(80, 16);
+            this.uitleg_game.TabIndex = 6;
+            this.uitleg_game.Text = "Uitleg game";
+            // 
+            // Registratie_text
+            // 
+            this.Registratie_text.AutoSize = true;
+            this.Registratie_text.Location = new System.Drawing.Point(317, 9);
+            this.Registratie_text.Name = "Registratie_text";
+            this.Registratie_text.Size = new System.Drawing.Size(433, 16);
+            this.Registratie_text.TabIndex = 7;
+            this.Registratie_text.Text = "Om te kunnen deelnemen aan onze TechoV-Quiz mout u zich registreren";
+            // 
+            // voornaam_label
+            // 
+            this.voornaam_label.AutoSize = true;
+            this.voornaam_label.Location = new System.Drawing.Point(317, 81);
+            this.voornaam_label.Name = "voornaam_label";
+            this.voornaam_label.Size = new System.Drawing.Size(73, 16);
+            this.voornaam_label.TabIndex = 8;
+            this.voornaam_label.Text = "Voornaam:";
+            // 
+            // achternaam_label
+            // 
+            this.achternaam_label.AutoSize = true;
+            this.achternaam_label.Location = new System.Drawing.Point(317, 128);
+            this.achternaam_label.Name = "achternaam_label";
+            this.achternaam_label.Size = new System.Drawing.Size(82, 16);
+            this.achternaam_label.TabIndex = 9;
+            this.achternaam_label.Text = "Achternaam:";
+            // 
+            // email_label
+            // 
+            this.email_label.AutoSize = true;
+            this.email_label.Location = new System.Drawing.Point(317, 216);
+            this.email_label.Name = "email_label";
+            this.email_label.Size = new System.Drawing.Size(93, 16);
+            this.email_label.TabIndex = 10;
+            this.email_label.Text = "E-mail adress:";
+            // 
+            // leeftijd_label
+            // 
+            this.leeftijd_label.AutoSize = true;
+            this.leeftijd_label.Location = new System.Drawing.Point(317, 172);
+            this.leeftijd_label.Name = "leeftijd_label";
+            this.leeftijd_label.Size = new System.Drawing.Size(53, 16);
+            this.leeftijd_label.TabIndex = 11;
+            this.leeftijd_label.Text = "Leeftijd:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(317, 260);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 12;
+            // 
+            // gsm_label
+            // 
+            this.gsm_label.AutoSize = true;
+            this.gsm_label.Location = new System.Drawing.Point(317, 260);
+            this.gsm_label.Name = "gsm_label";
+            this.gsm_label.Size = new System.Drawing.Size(92, 16);
+            this.gsm_label.TabIndex = 13;
+            this.gsm_label.Text = "GSM-nummer:";
+            // 
+            // registratie_btn
+            // 
+            this.registratie_btn.Location = new System.Drawing.Point(474, 343);
+            this.registratie_btn.Name = "registratie_btn";
+            this.registratie_btn.Size = new System.Drawing.Size(92, 40);
+            this.registratie_btn.TabIndex = 14;
+            this.registratie_btn.Text = "Registreren";
+            this.registratie_btn.UseVisualStyleBackColor = true;
+            // 
+            // gsm_pakker_als_er_mooie_mamas_zijn
+            // 
+            this.gsm_pakker_als_er_mooie_mamas_zijn.AutoSize = true;
+            this.gsm_pakker_als_er_mooie_mamas_zijn.Location = new System.Drawing.Point(212, 372);
+            this.gsm_pakker_als_er_mooie_mamas_zijn.Name = "gsm_pakker_als_er_mooie_mamas_zijn";
+            this.gsm_pakker_als_er_mooie_mamas_zijn.Size = new System.Drawing.Size(254, 20);
+            this.gsm_pakker_als_er_mooie_mamas_zijn.TabIndex = 15;
+            this.gsm_pakker_als_er_mooie_mamas_zijn.Text = "Ik wil meldingen krijgen over TechnoV";
+            this.gsm_pakker_als_er_mooie_mamas_zijn.ThreeState = true;
+            this.gsm_pakker_als_er_mooie_mamas_zijn.UseVisualStyleBackColor = true;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.gsm_pakker_als_er_mooie_mamas_zijn);
+            this.Controls.Add(this.registratie_btn);
+            this.Controls.Add(this.gsm_label);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.leeftijd_label);
+            this.Controls.Add(this.email_label);
+            this.Controls.Add(this.achternaam_label);
+            this.Controls.Add(this.voornaam_label);
+            this.Controls.Add(this.Registratie_text);
+            this.Controls.Add(this.uitleg_game);
+            this.Controls.Add(this.gsm_textbox);
+            this.Controls.Add(this.email_textbox);
+            this.Controls.Add(this.achternaam_textbox);
+            this.Controls.Add(this.leeftijd_textbox);
+            this.Controls.Add(this.voornaam_textbox);
+            this.Controls.Add(this.Titel);
+            this.Name = "Form";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Titel;
+        private System.Windows.Forms.TextBox voornaam_textbox;
+        private System.Windows.Forms.TextBox leeftijd_textbox;
+        private System.Windows.Forms.TextBox achternaam_textbox;
+        private System.Windows.Forms.TextBox email_textbox;
+        private System.Windows.Forms.TextBox gsm_textbox;
+        private System.Windows.Forms.Label uitleg_game;
+        private System.Windows.Forms.Label Registratie_text;
+        private System.Windows.Forms.Label voornaam_label;
+        private System.Windows.Forms.Label achternaam_label;
+        private System.Windows.Forms.Label email_label;
+        private System.Windows.Forms.Label leeftijd_label;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label gsm_label;
+        private System.Windows.Forms.Button registratie_btn;
+        private System.Windows.Forms.CheckBox gsm_pakker_als_er_mooie_mamas_zijn;
     }
 }
 
--- Frontend/RegistratieProgram/Form1.cs
+++ Frontend/RegistratieProgram/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace RegistratieProgram
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        public Form()
         {
             InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
