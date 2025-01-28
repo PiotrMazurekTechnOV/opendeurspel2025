c83e808bfa523a1de9e1a7795f023aebd7554e7f
Denis <denis.hristov@leerling.kov.be>
2025-01-28 11:41:45 +0100

    commit want het moest, kleur verandering lokaalchooser

--- Frontend/QuizUI/QuizUI/lokaalchooser.Designer.cs
+++ Frontend/QuizUI/QuizUI/lokaalchooser.Designer.cs
@@ -71,6 +71,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Peru;
             this.ClientSize = new System.Drawing.Size(484, 211);
             this.Controls.Add(this.EnterBtn);
             this.Controls.Add(this.textBox1);
