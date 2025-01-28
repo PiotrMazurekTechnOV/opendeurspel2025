c3632c906b40947ba98ebb805ef891b3e0222130
Alexi <alexi.anthonis@leerling.kov.be>
2025-01-28 12:39:14 +0100

    API
    api

---
+++ Backend/Serverconnection.js
@@ -0,0 +1,9 @@
+require('http').createServer(function(req,res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    res.end('' + process.pid);
+
+}).listen(3000);
